47d39e0d1246f5154aacbbb564e82ab4fa782354
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-04-04 11:25:34 +0200

    Both View sets refactored to useing django-filters module with parameters. All action functions working. Further tests needed.

--- Car_owners/application/views.py
+++ Car_owners/application/views.py
@@ -24,19 +24,22 @@ class OwnerViewSet(viewsets.ModelViewSet):
 
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('name', 'surname')
 
-    @action(detail=False, url_path=r'name/(?P<name>\w+)')
-    def owners_with_the_given_name(self, request, name):
-        owners = Owner.objects.filter(name=name.title())
-        return check_if_queryset_is_empty(owners, 'owners', 'name',
-                                          name.title(),
+    @action(detail=False, url_path='name')
+    def owners_with_the_given_name(self, request):
+        name = request.GET['name'].title()
+        owners = Owner.objects.filter(name=name)
+        return check_if_queryset_is_empty(owners, 'owners', 'name', name,
                                           self.get_serializer, many=True)
 
-    @action(detail=False, url_path=r'surname/(?P<surname>\w+)')
-    def owners_with_the_given_surname(self, request, surname):
-        owners = Owner.objects.filter(surname=surname.title())
+    @action(detail=False, url_path='surname')
+    def owners_with_the_given_surname(self, request):
+        surname = request.GET['surname'].title()
+        owners = Owner.objects.filter(surname=surname)
         return check_if_queryset_is_empty(owners, 'owners', 'surname',
-                                          surname.title(), self.get_serializer,
+                                          surname, self.get_serializer,
                                           many=True)
 
     @action(detail=False,
@@ -54,16 +57,19 @@ class CarViewSet(viewsets.ModelViewSet):
     """
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('brand', 'model')
 
-    @action(detail=False, url_path=r'brand/(?P<brand>\w+)')
-    def cars_with_the_given_brand(self, request, brand):
+    @action(detail=False, url_path='brand')
+    def cars_with_the_given_brand(self, request):
+        brand = request.GET['brand'].title()
         cars = Car.objects.filter(brand=brand)
         return check_if_queryset_is_empty(cars, 'cars', 'brand', brand,
                                           self.get_serializer, many=True)
 
-    # To further debugging
-    @action(detail=False, url_path=r'model/(?P<model>\w+)')
-    def cars_with_the_given_model(self, request, model):
+    @action(detail=False, url_path='model')
+    def cars_with_the_given_model(self, request):
+        model = request.GET['model']
         cars = Car.objects.filter(model=model)
         return check_if_queryset_is_empty(cars, 'cars', 'model', model,
                                           self.get_serializer, many=True)
@@ -73,16 +79,4 @@ class CarViewSet(viewsets.ModelViewSet):
     def cars_in_alphabetic_order(self, request, brand_or_model):
         cars = Car.objects.all().order_by(brand_or_model)
         serializer = self.get_serializer(cars, many=True)
-        return Response(serializer.data)
-
-
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ('brand',)
-
-    @action(detail=False,
-            url_path='test')
-    def test_function(self, request):
-        cars = Car.objects.filter(brand=request.GET['brand'])
-        serializer = self.get_serializer(cars, many=True)
-        print(request.GET['brand'])
         return Response(serializer.data)
