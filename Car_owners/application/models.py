e79a0decfb6db298f417cae4117b5cf08d365348
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-12 09:37:55 +0200

    Correct models.py and .gitignore files
    Add blank new line to .gitigonre file. At models.py simplified phone field validation - get rid of 3rd part library, Extend max length for car's model field and delete overwritten save() function.

--- Car_owners/application/models.py
+++ Car_owners/application/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField # a bit of an overkill to use a 3rd party lib for such trivial field
 
 
 class Owner(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
-    phone = PhoneNumberField(blank=True, unique=True)
+    phone = models.CharField(max_length=9, unique=True, blank=True)
 
     def __str__(self):
         return f"{self.name} {self.surname}"
@@ -13,14 +12,9 @@ class Owner(models.Model):
 
 class Car(models.Model):
     brand = models.CharField(max_length=20)
-    model = models.CharField(max_length=20) # I don't like this limit, I own a Lamborghini Aventador LP 750-4 Superveloce 
+    model = models.CharField(max_length=40)
     production_date = models.DateField()
     owner = models.ForeignKey("Owner", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.brand} {self.model}"
-
-    def save(self, *args, **kwargs): # since you overwrite default `save` method you could actually significantly change the way it works, like implementing a `last_changed` field and setting it here based on current datetime
-        self.brand = self.brand.title()
-        self.model = self.model.title()
-        super(Car, self).save(*args, **kwargs)
