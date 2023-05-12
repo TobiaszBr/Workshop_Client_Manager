f2c6b09c3b5d67f188f660fb02d80e1167d20002
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-12 16:24:55 +0200

    Correct views.py
    get_parameters_from_request function - part 2 - function simplified by using request.query_params. Other function owner_parameters_search_logic - simplified by making one queryset 'owners' based on given data from request.query_params. Code refactored.

--- Car_owners/application/views.py
+++ Car_owners/application/views.py
@@ -6,22 +6,6 @@ from .models import Owner, Car
 from .serializers import OwnerSerializer, CarSerializer
 
 
-def check_if_queryset_is_empty(
-    queryset,
-    objects,
-    parameter_to_check_input,
-    serializer,
-    *parameter_to_check,
-    many=False,
-): # instead of using such custom function, you should use Django ORM builtin methods. Just set a filter for parameters you want to check against, prepare a queryset and use .exists()
-    if queryset:
-        serializer = serializer(queryset, many=many)
-        return Response(serializer.data)
-
-    parameters = " ".join([parameter for parameter in parameter_to_check]).strip()
-    return Response(
-        f"There is no {objects} with {parameters}" f" {parameter_to_check_input}"
-    )
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
@@ -29,85 +13,39 @@ class OwnerViewSet(viewsets.ModelViewSet):
     This viewset automatically provides 'list', 'create', 'retrieve',
     'update', and 'destroy' actions.
     """
-
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
 
     def get_parameters_from_request(self, request):
+        request_data_dict = {}
+        for item in request.query_params:
+            if item == "name" or item == "surname":
+                request_data_dict[item] = request.query_params[item].capitalize().strip()
+            else:
+                request_data_dict[item] = request.query_params[item].strip()
 
-        name = request.GET.get("name", "").capitalize().strip()
-        surname = request.GET.get("surname", "").capitalize().strip()
-        phone = "+" + request.GET.get("phone", "").strip()       # this screams "there is no validation for phone field in app if we let it be one digit"
+        return request_data_dict
 
+    def owner_parameters_search_logic(self, request_data_dict): # a really good habit, especially since python 3.10 is typing. https://docs.python.org/3/library/typing.html
 
-        return name, surname, phone
-
-    def owner_parameters_search_logic(self, name, surname, phone): # a really good habit, especially since python 3.10 is typing. https://docs.python.org/3/library/typing.html
-        if phone: # instead of all these if conditionals, you should create one generic method to create a queryset based on provided parameters. .filter and request.query_params will help you do just that.
-            try:
-                owner = Owner.objects.get(phone=phone) # get returns one result. Maybe it's better to create owner_qs and use .filter() ?
-                if (
-                    not name
-                    and not surname
-                    or name == owner.name
-                    and surname == owner.surname
-                    or name == owner.name
-                    and not surname
-                    or surname == owner.surname
-                    and not name
-                ): # way too complex for one conditional. And I'm not sure what the idea behind it is. Is it all possible connections between name, surname and phone? You can just include that in one ORM filter, check for a specific entry in db that meets all the requirements.
-                    owners = owner
-                else:
-                    owners = 0 # type mismatch, in line 71 you set `owners` to be a queryset, here it's an int. Typing will help you avoid such situations
-            except Owner.DoesNotExist:
-                owners = Owner.objects.filter(phone=phone) # not sure what this means. If you can't get an entry in the database for specific phone number, you want to return an empty queryset - that's fine but using this filter is pointless since you already know there isn't an entry that matches this phone.
-
-            name_prompt = "name " if name else ""
-            surname_prompt = "surname " if surname else ""
-            and_prompt = "and " if name or surname else "" # wtf is this :D get rid of this, if you want to prepare an information for user about an entry existing in the database or not, do it in one method that combines provided parameters into one message, use f-string.
-
-            return check_if_queryset_is_empty(
-                owners,
-                "owner",
-                f"{name_prompt}{surname_prompt}" f"{and_prompt}phone",
-                self.get_serializer,
-                *[name, surname, phone], # you unpack a list that is unpacked and iterated over down the function
-                many=False,
-            )
-
-        elif name and surname:
-            owners = Owner.objects.filter(name=name, surname=surname)
-            return check_if_queryset_is_empty(
-                owners,
-                "owner",
-                "name and surname",
-                self.get_serializer,
-                *[name, surname],
-                many=True,
-            )
-
-        elif name:
-            owners = Owner.objects.filter(name=name)
-            return check_if_queryset_is_empty(
-                owners, "owner", "name", self.get_serializer, name, many=True
-            )
-
-        elif surname:
-            owners = Owner.objects.filter(surname=surname)
-            return check_if_queryset_is_empty(
-                owners, "owner", "surname", self.get_serializer, surname, many=True
-            )
+        owners = Owner.objects.filter(**request_data_dict)
+        serializer = self.get_serializer(owners, many=True)
+        # owners.exists() do zapytania czy może zostać samo owners - jaka różnica
+        if owners:
+            return Response(serializer.data)
         else:
-            return Response({"You did not put any parameter to search " "function."}) # end user shouldn't know anything about "search function". This function should be executed only if there are any query params in url, so there is no use case where params are provided but this function is executed. And we're inside a class so it's better to use name "method"
+            return Response("There is no owner with given data.")
+
+
 
     # additional action functions.
     @action(detail=False, url_path="search")
     def owners_with_the_given_data(self, request):
-        # Try to get parameters from URL request
-        name, surname, phone = self.get_parameters_from_request(request)
+        # Get parameters from URL request
+        request_data_dict = self.get_parameters_from_request(request)
 
         # Owner's parameters search and filter logic with responds.
-        return self.owner_parameters_search_logic(name, surname, phone)
+        return self.owner_parameters_search_logic(request_data_dict)
 
 
 
