fe838c1e47aac7f645950bfd4ab453a9a8ead472
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-13 21:30:56 +0200

    Correct views.py
    Added BaseViewSet from which both OwnerViewSet and CarViewSet inherit now. Code refactored.

--- Car_owners/application/views.py
+++ Car_owners/application/views.py
@@ -6,47 +6,69 @@ from .models import Owner, Car
 from .serializers import OwnerSerializer, CarSerializer
 from re import search
 
+# a really good habit, especially since python 3.10 is typing. https://docs.python.org/3/library/typing.html
 
-class OwnerViewSet(viewsets.ModelViewSet):
+class BaseViewSet(viewsets.ModelViewSet):
     """
-    This viewset automatically provides 'list', 'create', 'retrieve',
-    'update', and 'destroy' actions.
+        This viewset automatically provides 'list', 'create', 'retrieve',
+        'update', and 'destroy' actions.
     """
-    queryset = Owner.objects.all()
-    serializer_class = OwnerSerializer
-                                                                                         # a really good habit, especially since python 3.10 is typing. https://docs.python.org/3/library/typing.html
-    def get_parameters_from_request(self, request, *elements_to_capitalize_list):
+
+    elements_to_capitalize_list = []
+    model_class_name = "object"
+    model_class = Owner
+    additional_validation = False
+
+    def get_parameters_from_request(self, request):
         request_data_dict = {}
         for item in request.query_params:
-            if item in elements_to_capitalize_list:
+            if item in self.elements_to_capitalize_list:
                 request_data_dict[item] = request.query_params[item].capitalize().strip()
             else:
                 request_data_dict[item] = request.query_params[item].strip()
 
         return request_data_dict
 
-
-    def response_when_no_owner_found(self, request_data_dict):
-        respond = "There is no owner with"
+    def response_when_no_object_found(self, request_data_dict):
+        respond = f"There is no {self.model_class_name} with"
         for key, value in request_data_dict.items():
             respond += f" {key}: {value}"
 
         return Response(f"{respond}.")
 
+    def additional_validation_check(self, request_data_dict):
+        return self.additional_validation, Response(
+            {"Additional validation error occured."})
+
     # additional action functions.
     @action(detail=False, url_path="search")
-    def owners_with_the_given_data(self, request):
+    def object_with_the_given_data(self, request):
         # Get parameters from URL request
-        request_data_dict = self.get_parameters_from_request(request, "name", "surname")
+        request_data_dict = self.get_parameters_from_request(request)
 
-        # Filters owners with given data and respond.
-        owners = Owner.objects.filter(**request_data_dict)
-        serializer = self.get_serializer(owners, many=True)
-                                                                                            # owners.exists() do zapytania czy może zostać samo owners - jaka różnica
-        if owners:
+        # Additional validation
+        validation_error, response = self.additional_validation_check(request_data_dict)
+        if validation_error:
+            return response
+
+        # Filters objects with given data and respond.
+        query_set = self.model_class.objects.filter(**request_data_dict)
+        serializer = self.get_serializer(query_set, many=True)
+                                                                                            # objects.exists() do zapytania czy może zostać samo owners - jaka różnica
+        if query_set:
             return Response(serializer.data)
         else:
-            return self.response_when_no_owner_found(request_data_dict)
+            return self.response_when_no_object_found(request_data_dict)
+
+
+class OwnerViewSet(BaseViewSet):
+
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+    elements_to_capitalize_list = ["name", "surname"]
+    model_class_name = "owner"
+    model_class = Owner
 
     @action(detail=False, url_path=r"(?P<name_or_surname>[name surname]+)/alphabetic")
     def owners_in_alphabetic_order(self, request, name_or_surname):
@@ -55,56 +77,23 @@ class OwnerViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class CarViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides 'list', 'create', 'retrieve',
-    'update', and 'destroy' actions.
-    """
+class CarViewSet(BaseViewSet):
 
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
-    # all of my comments from OwnerViewSet also apply to this class. But you should see many repetitions of code that looks exactly the same. Make use of the fact it's OOP and use inheritance + mixins
+    elements_to_capitalize_list = ["brand"]
+    model_class_name = "car"
+    model_class = Car
+    additional_validation = True
 
-    # To samo co w owners
-    def get_parameters_from_request(self, request, *elements_to_capitalize_list):
-        request_data_dict = {}
-        for item in request.query_params:
-            if item in elements_to_capitalize_list:
-                request_data_dict[item] = request.query_params[item].capitalize().strip()
-            else:
-                request_data_dict[item] = request.query_params[item].strip()
-
-        return request_data_dict
-
-    # To samo co w owners
-    def response_when_no_owner_found(self, request_data_dict):
-        respond = "There is no owner with"
-        for key, value in request_data_dict.items():
-            respond += f" {key}: {value}"
-
-        return Response(f"{respond}.")
-
-    @action(detail=False, url_path="search")
-    def cars_with_the_given_data(self, request):
-        # Get parameters from URL request
-        request_data_dict = self.get_parameters_from_request(request, "brand")
-
-
-        # Data validation
+    def additional_validation_check(self, request_data_dict):
         # Validate the production date variable
-        if not search("[\d][\d][\d][\d][-][\d][\d][-][\d][\d]", request_data_dict.get("production_date", "2000-01-01")):
-            return Response({"Date must be in YYYY-MM-DD format."})
+        if not search("[\d][\d][\d][\d][-][\d][\d][-][\d][\d]",
+                      request_data_dict.get("production_date", "2000-01-01")):
+            return True, Response({"Date must be in YYYY-MM-DD format."})
+        return False, Response({""})
 
-
-        # Filters owners with given data and respond.
-        cars = Car.objects.filter(**request_data_dict)
-        serializer = self.get_serializer(cars, many=True)
-        # owners.exists() do zapytania czy może zostać samo owners - jaka różnica
-        if cars:
-            return Response(serializer.data)
-        else:
-            return self.response_when_no_owner_found(request_data_dict)
 
 
     @action(detail=False, url_path=r"(?P<brand_or_model>[brand model]+)/alphabetic")
