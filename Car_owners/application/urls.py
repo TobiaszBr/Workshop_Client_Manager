0d4e53a6847a32811670bb3bb37b6f2f961b791a
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-22 14:37:30 +0200

    The first commit with new swagger interface - used drf-yasg module

--- Car_owners/application/urls.py
+++ Car_owners/application/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from . import views
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Car owners API",
+      default_version='v1',
+      description="Test description - test",
+      terms_of_service="https://www.test.com/policies/terms/",
+      contact=openapi.Contact(email="contact@test.local"),
+      license=openapi.License(name="TEST License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 # Create a router and register our viewsets with it.
@@ -10,5 +27,6 @@ router.register(r"cars", views.CarViewSet, basename="car")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path("", include(router.urls)),
+    path("app/", include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- Car_owners/car_owners/settings.py
+++ Car_owners/car_owners/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "application",
     "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
