40bce8b77bd85092bb99d7bfcaaa6445a922d8a3
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-22 10:24:52 +0200

    Case-insensitive search for car's brand and model added

--- Car_owners/application/views.py
+++ Car_owners/application/views.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from re import search
 from typing import Dict
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.decorators import action
 import rest_framework.request
@@ -8,9 +9,6 @@ from rest_framework.response import Response
 from .models import Owner, Car
 from .serializers import OwnerSerializer, CarSerializer
 
-from django.db.models import Q
-
-from django.db.models.fields import CharField
 
 request_type = rest_framework.request.Request
 response_type = rest_framework.response.Response
@@ -28,7 +26,6 @@ class BaseViewSet(ABC, viewsets.ModelViewSet):
         self.elements_to_capitalize_list = []
         self.model_class_name = "object"
         self.model_class = None
-        self.additional_validation = False
 
 
     def response_when_no_object_found(self, request: request_type) -> response_type:
@@ -42,33 +39,6 @@ class BaseViewSet(ABC, viewsets.ModelViewSet):
     @abstractmethod
     def objects_with_the_given_data(self):
         pass
-
-    """def objects_with_the_given_data(
-        self, request: request_type, change_first_char_case: bool = False
-    ) -> response_type:
-        # Get parameters from URL request
-        request_data_dict = self.get_parameters_from_request(
-            request, change_first_char_case)
-
-        # Additional validation
-        validation_error, response = self.additional_validation_check(request_data_dict)
-        if validation_error:
-            return response
-
-        # Filters objects with given data and respond.
-        query_set = self.model_class.objects.filter(**request_data_dict)
-        serializer = self.get_serializer(query_set, many=True)
-
-        # Return data or change first letters
-        if query_set:
-            return Response(serializer.data)
-        elif not change_first_char_case:
-            return self.objects_with_the_given_data(
-                request, change_first_char_case=True                                        # tu niby to działa, ale jak zmienię np tobiasz&Bernacki to już nie (bo zawsze zmieniam oba parametry, a w sumie powinienem zmienić pojedynczo i szukać
-                                                                                            # czyli w tym przypadku dla name i surname w sumie powinno szukać 4 razy?
-            )
-        else:
-            return self.response_when_no_object_found(request)"""
 
     @abstractmethod
     def objects_in_alphabetical_order(self):
@@ -85,7 +55,6 @@ class OwnerViewSet(BaseViewSet):
         self.elements_to_capitalize_list = ["name", "surname"]
         self.model_class_name = "owner"
         self.model_class = Owner
-        self.additional_validation = False
 
     # additional action functions.
     @action(detail=False, url_path="search")
@@ -114,8 +83,6 @@ class OwnerViewSet(BaseViewSet):
         else:
             return self.response_when_no_object_found(request)
 
-
-
     @action(detail=False, url_path=r"alphabetical/(?P<alphabetical_base>[name surname]+)")
     def objects_in_alphabetical_order(self, request: request_type, alphabetical_base: str
     ) -> response_type:
@@ -135,9 +102,8 @@ class CarViewSet(BaseViewSet):
         self.elements_to_capitalize_list = ["brand", "model"]
         self.model_class_name = "car"
         self.model_class = Car
-        self.additional_validation = True
 
-    def additional_validation_check(self, request_data_dict: Dict[str, str]
+    def additional_validation_check(self, request_data_dict: Dict[str, str]                         # jak jest np 2022-05-32 to jest format ok ale data nie valid i jest błąd
     ) -> (bool, response_type):
         # Validate the production date variable
         if not search(
@@ -146,6 +112,38 @@ class CarViewSet(BaseViewSet):
         ):
             return True, Response({"Date must be in YYYY-MM-DD format."})
         return False, Response({""})
+
+    # additional action functions.
+    @action(detail=False, url_path="search")
+    def objects_with_the_given_data(self, request: request_type) -> response_type:
+        # Additional validation
+        validation_error, response = self.additional_validation_check(request.query_params)
+        if validation_error:
+            return response
+
+        # Filters objects with given data and respond.
+        filter_list = []
+        if "brand" in request.query_params.keys():
+            q = Q(brand__iexact=request.query_params["brand"])
+            filter_list.append(q)
+        if "model" in request.query_params.keys():
+            q = Q(model__iexact=request.query_params["model"])
+            filter_list.append(q)
+        if "production_date" in request.query_params.keys():
+            q = Q(production_date=request.query_params["production_date"])
+            filter_list.append(q)
+        if "owner" in request.query_params.keys():
+            q = Q(owner=request.query_params["owner"])
+            filter_list.append(q)
+
+        query_set = self.model_class.objects.filter(*filter_list)
+        serializer = self.get_serializer(query_set, many=True)
+
+        # Return data or change first letters
+        if query_set:
+            return Response(serializer.data)
+        else:
+            return self.response_when_no_object_found(request)
 
     @action(detail=False, url_path=r"production_date/(?P<sort_order>[ascending descending]+)")
     def cars_in_production_date_order(self, request: request_type, sort_order: str) -> response_type:
@@ -164,4 +162,3 @@ class CarViewSet(BaseViewSet):
         query_set = self.model_class.objects.all().order_by(alphabetical_base)
         serializer = self.get_serializer(query_set, many=True)
         return Response(serializer.data)
-
