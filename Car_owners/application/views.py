9d511a1655bea1281e16b9ef53a9a1dc1014010c
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-05-22 12:50:09 +0200

    Make search functionality universal once again
    Thanks to get_filter_list it was possible to make object_with_the_given_data search function universal again. Added missing typing hints. Code cleared.

--- Car_owners/application/views.py
+++ Car_owners/application/views.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Dict
-from django.db.models import Q
+from typing import Dict, List
+from django.db.models import Q, query_utils
 from rest_framework import viewsets
 from rest_framework.decorators import action
 import rest_framework.request
@@ -12,6 +12,7 @@ from .serializers import OwnerSerializer, CarSerializer
 
 request_type = rest_framework.request.Request
 response_type = rest_framework.response.Response
+q_type = query_utils.Q
 
 
 class BaseViewSet(ABC, viewsets.ModelViewSet):
@@ -25,6 +26,8 @@ class BaseViewSet(ABC, viewsets.ModelViewSet):
 
         self.model_class = None
         self.model_class_name = "object"
+        self.queryset = None
+        self.case_insensitive_fields = []
 
     def response_when_no_object_found(self, request: request_type) -> response_type:
         respond = f"There is no {self.model_class_name} with"
@@ -33,10 +36,45 @@ class BaseViewSet(ABC, viewsets.ModelViewSet):
 
         return Response(f"{respond}.")
 
+    def get_filter_list(self, request: request_type) -> List[q_type]:
+        filter_list = []
+        model_fields_names_list = [element.name for element in self.model_class._meta.get_fields()]
+
+        for field in model_fields_names_list:
+            if field in request.query_params.keys():
+                parameters_dict = {}
+                if field in self.case_insensitive_fields:
+                    parameters_dict[f"{field}__iexact"] = request.query_params[field]
+                else:
+                    parameters_dict[field] = request.query_params[field]
+
+                q = Q(**parameters_dict)
+                print(q)
+                print(type(q))
+                filter_list.append(q)
+
+        return filter_list
+
+    def additional_validation_check(self, request_data_dict: Dict[str, str]) -> (bool, response_type):
+        return False, Response({""})
+
     # additional action functions.
-    @abstractmethod
-    def objects_with_the_given_data(self):
-        pass
+    @action(detail=False, url_path="search")
+    def objects_with_the_given_data(self, request: request_type) -> response_type:
+        # Additional validation
+        validation_error, response = self.additional_validation_check(request.query_params)
+        if validation_error:
+            return response
+
+        filter_list = self.get_filter_list(request)
+        queryset = self.queryset.filter(*filter_list)
+        serializer = self.get_serializer(queryset, many=True)
+
+        # Return data or change first letters
+        if queryset:
+            return Response(serializer.data)
+        else:
+            return self.response_when_no_object_found(request)
 
     @abstractmethod
     def objects_in_alphabetical_order(self):
@@ -53,45 +91,20 @@ class OwnerViewSet(BaseViewSet):
         self.model_class = Owner
         self.model_class_name = self.model_class._meta.object_name
         self.queryset = self.model_class.objects.all()
+        self.case_insensitive_fields = ["name", "surname"]
 
     # additional action functions.
-    @action(detail=False, url_path="search")
-    def objects_with_the_given_data(self, request: request_type) -> response_type:
-        # Filters objects with given data and respond.
-        filter_list = []
-
-        if "name" in request.query_params.keys():                                           # postaraj się zrobić to bardziej uniwersalnie?
-            q = Q(name__iexact = request.query_params["name"])
-            filter_list.append(q)
-
-        if "surname" in request.query_params.keys():
-            q = Q(surname__iexact=request.query_params["surname"])
-            filter_list.append(q)
-
-        if "phone" in request.query_params.keys():
-            q = Q(phone=request.query_params["phone"])
-            filter_list.append(q)
-
-        queryset = self.queryset.filter(*filter_list)
-        serializer = self.get_serializer(queryset, many=True)
-
-        # Return data or change first letters
-        if queryset:
-            return Response(serializer.data)
-        else:
-            return self.response_when_no_object_found(request)
-
     @action(detail=False, url_path=r"alphabetical/(?P<alphabetical_base>[name surname]+)")
     def objects_in_alphabetical_order(self, request: request_type, alphabetical_base: str
     ) -> response_type:
 
-        query_set = self.model_class.objects.all().order_by(alphabetical_base)
+        query_set = self.queryset.all().order_by(alphabetical_base)
         serializer = self.get_serializer(query_set, many=True)
         return Response(serializer.data)
 
 
 class CarViewSet(BaseViewSet):
-    
+
     serializer_class = CarSerializer
 
     def __init__(self, *args, **kwargs):
@@ -100,6 +113,7 @@ class CarViewSet(BaseViewSet):
         self.model_class = Car
         self.model_class_name = self.model_class._meta.object_name
         self.queryset = self.model_class.objects.all()
+        self.case_insensitive_fields = ["brand", "model"]
 
     def additional_validation_check(self, request_data_dict: Dict[str, str]
     ) -> (bool, response_type):
@@ -113,45 +127,13 @@ class CarViewSet(BaseViewSet):
 
         return False, Response({""})
 
-
     # additional action functions.
-    @action(detail=False, url_path="search")
-    def objects_with_the_given_data(self, request: request_type) -> response_type:
-        # Additional validation
-        validation_error, response = self.additional_validation_check(request.query_params)
-        if validation_error:
-            return response
-
-        # Filters objects with given data and respond.
-        filter_list = []
-        if "brand" in request.query_params.keys():
-            q = Q(brand__iexact=request.query_params["brand"])
-            filter_list.append(q)
-        if "model" in request.query_params.keys():
-            q = Q(model__iexact=request.query_params["model"])
-            filter_list.append(q)
-        if "production_date" in request.query_params.keys():
-            q = Q(production_date=request.query_params["production_date"])
-            filter_list.append(q)
-        if "owner" in request.query_params.keys():
-            q = Q(owner=request.query_params["owner"])
-            filter_list.append(q)
-
-        queryset = self.queryset.filter(*filter_list)
-        serializer = self.get_serializer(queryset, many=True)
-
-        # Return data or change first letters
-        if queryset:
-            return Response(serializer.data)
-        else:
-            return self.response_when_no_object_found(request)
-
     @action(detail=False, url_path=r"production_date/(?P<sort_order>[ascending descending]+)")
     def cars_in_production_date_order(self, request: request_type, sort_order: str) -> response_type:
         if sort_order == "ascending":
-            query_set = self.model_class.objects.all().order_by("production_date")
+            query_set = self.queryset.all().order_by("production_date")
         elif sort_order == "descending":
-            query_set = self.model_class.objects.all().order_by("-production_date")
+            query_set = self.queryset.all().order_by("-production_date")
         else:
             return Response({"Wrong url."})
 
@@ -160,6 +142,6 @@ class CarViewSet(BaseViewSet):
 
     @action(detail=False, url_path=r"alphabetical/(?P<alphabetical_base>[brand model]+)")
     def objects_in_alphabetical_order(self, request: request_type, alphabetical_base: str) -> response_type:
-        query_set = self.model_class.objects.all().order_by(alphabetical_base)
+        query_set = self.queryset.all().order_by(alphabetical_base)
         serializer = self.get_serializer(query_set, many=True)
         return Response(serializer.data)
