33b352fa2666521a39cfce8000b0b7270e96e6bf
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-07-03 10:04:50 +0200

    Removed venv creation - it is not needed

--- Car_owners/car_owners/prod_settings.py
+++ Car_owners/car_owners/prod_settings.py
@@ -66,9 +66,9 @@ WSGI_APPLICATION = "car_owners.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
         "HOST": "db",
         "PORT": "5432",
     }
