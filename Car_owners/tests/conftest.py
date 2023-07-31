140b47ff476ac3ddcaa290b9b5cd93f4ed70a049
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-07-31 17:12:17 +0200

    Car view tests corrected according to code review remarks

--- Car_owners/tests/conftest.py
+++ Car_owners/tests/conftest.py
@@ -9,6 +9,7 @@ def valid_owner_data() -> Owner:
     owner_data = {"name": "Andrzej", "surname": "Starczyk", "phone": "123456789"}
     return owner_data
 
+
 @pytest.fixture
 def valid_new_owner_data() -> Owner:
     owner_data = {"name": "Tadeusz", "surname": "Madej", "phone": "789456789"}
@@ -34,7 +35,8 @@ def valid_car_serializer_data(
 
 
 @pytest.fixture
-def valid_car_model_data(valid_car_serializer_data: dict[str, str | datetime.date | Owner],
+def valid_car_model_data(
+    valid_car_serializer_data: dict[str, str | datetime.date | Owner],
 ) -> Car:
     car = Car.objects.create(**valid_car_serializer_data)
     return car
@@ -56,6 +58,7 @@ def valid_car_view_data(valid_owner_model_data: Owner) -> dict[str, str | int]:
     }
 
     return car_data
+
 
 @pytest.fixture
 def valid_new_car_view_data(valid_owner_model_data: Owner) -> dict[str, str | int]:
--- Car_owners/tests/test_views.py
+++ Car_owners/tests/test_views.py
@@ -6,9 +6,10 @@ from application.models import Owner, Car
 from application.views import OwnerViewSet, CarViewSet
 
 
-class TestsOwnerViews():
+class TestsOwnerViews:
     @pytest.mark.django_db
-    def test_create_owner(self, api_client: APIClient, valid_owner_data: dict[str, str]
+    def test_create_owner(
+        self, api_client: APIClient, valid_owner_data: dict[str, str]
     ) -> None:
         # create owner
         response_create_owner = api_client.post(
@@ -23,7 +24,8 @@ class TestsOwnerViews():
         )
 
     @pytest.mark.django_db
-    def test_get_owner(self, api_client: APIClient, valid_owner_model_data: Owner
+    def test_get_owner(
+        self, api_client: APIClient, valid_owner_model_data: Owner
     ) -> None:
         owner_id = valid_owner_model_data.id
         # get owner
@@ -32,13 +34,19 @@ class TestsOwnerViews():
         del valid_owner_model_data_dict["_state"]
         assert response_get_owner.status_code == status.HTTP_200_OK
         assert all(
-            [response_get_owner.data[key] == valid_owner_model_data_dict[key] for key in
-             valid_owner_model_data_dict.keys()]
+            [
+                response_get_owner.data[key] == valid_owner_model_data_dict[key]
+                for key in valid_owner_model_data_dict.keys()
+            ]
         )
 
     @pytest.mark.django_db
-    def test_patch_owner(self, api_client: APIClient, valid_owner_model_data: Owner,
-            valid_new_owner_data: dict[str, str]) -> None:
+    def test_patch_owner(
+        self,
+        api_client: APIClient,
+        valid_owner_model_data: Owner,
+        valid_new_owner_data: dict[str, str],
+    ) -> None:
         # update owner
         owner_id = valid_owner_model_data.id
         response_patch_owner = api_client.patch(
@@ -46,8 +54,10 @@ class TestsOwnerViews():
         )
         assert response_patch_owner.status_code == status.HTTP_200_OK
         assert all(
-            [response_patch_owner.data[key] == valid_new_owner_data[key] for key in
-             valid_new_owner_data.keys()]
+            [
+                response_patch_owner.data[key] == valid_new_owner_data[key]
+                for key in valid_new_owner_data.keys()
+            ]
         )
 
         # owner does not exist
@@ -57,11 +67,14 @@ class TestsOwnerViews():
         assert response_patch_owner.status_code == status.HTTP_404_NOT_FOUND
 
     @pytest.mark.django_db
-    def test_delete_owner(self, api_client: APIClient, valid_owner_model_data: Owner) -> None:
+    def test_delete_owner(
+        self, api_client: APIClient, valid_owner_model_data: Owner
+    ) -> None:
         # delete owner
         owner_id = valid_owner_model_data.id
-        response_delete_owner = api_client.delete(f"/app/owners/{owner_id}/",
-                                                  format="json")
+        response_delete_owner = api_client.delete(
+            f"/app/owners/{owner_id}/", format="json"
+        )
         assert response_delete_owner.status_code == status.HTTP_204_NO_CONTENT
 
         # try to get that owner
@@ -74,23 +87,59 @@ class TestsOwnerViews():
         )
         assert response_delete_owner.status_code == status.HTTP_404_NOT_FOUND
 
-
-class TestsCarViews():
+    @pytest.mark.parametrize(
+        ("data", "expected_message"),
+        [
+            ({"phone": "asd1234cvb"}, "Phone number can contain only digits"),
+            ({"phone": "12345678911111"}, "Phone number is too long"),
+            ({"phone": "123"}, "Phone number is too short"),
+            (
+                {"name": "@#$"},
+                "name can contain only letters and '-' without whitespaces",
+            ),
+            (
+                {"surname": "@#$"},
+                "surname can contain only letters and '-' without whitespaces",
+            ),
+            (
+                {"ordering": "invalid ordering"},
+                f"Ordering should be one of the following: {', '.join(OwnerViewSet().ordering_fields)}",
+            ),
+        ],
+    )
     @pytest.mark.django_db
-    def test_create_car(self, api_client: APIClient, valid_car_view_data: dict[str, str | int]
+    def test_owner_request_validation(
+        self, api_client: APIClient, data: dict[str, str], expected_message: str
+    ) -> None:
+        ((key, value),) = data.items()
+        response_get_owner_invalid_data = api_client.get(
+            "/app/owners/", data=data, format="json"
+        )
+        assert (
+            response_get_owner_invalid_data.status_code == status.HTTP_400_BAD_REQUEST
+        )
+        assert response_get_owner_invalid_data.data[key] == expected_message
+
+
+class TestsCarViews:
+    @pytest.mark.django_db
+    def test_create_car(
+        self, api_client: APIClient, valid_car_view_data: dict[str, str | int]
     ) -> None:
         # create car
-        response_create_car = api_client.post("/app/cars/", data=valid_car_view_data,
-                                              format="json")
+        response_create_car = api_client.post(
+            "/app/cars/", data=valid_car_view_data, format="json"
+        )
         assert response_create_car.status_code == status.HTTP_201_CREATED
         assert all(
-            [response_create_car.data[key] == valid_car_view_data[key] for key in
-             valid_car_view_data.keys()]
+            [
+                response_create_car.data[key] == valid_car_view_data[key]
+                for key in valid_car_view_data.keys()
+            ]
         )
 
     @pytest.mark.django_db
-    def test_get_car(self, api_client: APIClient, valid_car_model_data: Car
-    ) -> None:
+    def test_get_car(self, api_client: APIClient, valid_car_model_data: Car) -> None:
         # get car
         car_id = valid_car_model_data.id
         response_get_car = api_client.get(f"/app/cars/{car_id}/", format="json")
@@ -100,11 +149,16 @@ class TestsCarViews():
         assert response_get_car.data["brand"] == valid_car_model_data.brand
         assert response_get_car.data["model"] == valid_car_model_data.model
         assert response_get_car.data["production_date"] == str(
-            valid_car_model_data.production_date)
+            valid_car_model_data.production_date
+        )
 
     @pytest.mark.django_db
-    def test_patch_car(self, api_client: APIClient, valid_car_model_data: Car,
-                       valid_new_car_view_data: dict[str, str | int]) -> None:
+    def test_patch_car(
+        self,
+        api_client: APIClient,
+        valid_car_model_data: Car,
+        valid_new_car_view_data: dict[str, str | int],
+    ) -> None:
         # update car
         car_id = valid_car_model_data.id
         response_patch_car = api_client.patch(
@@ -112,8 +166,10 @@ class TestsCarViews():
         )
         assert response_patch_car.status_code == status.HTTP_200_OK
         assert all(
-            [response_patch_car.data[key] == valid_new_car_view_data[key] for key in
-             valid_new_car_view_data.keys()]
+            [
+                response_patch_car.data[key] == valid_new_car_view_data[key]
+                for key in valid_new_car_view_data.keys()
+            ]
         )
 
         # car does not exist
@@ -134,94 +190,63 @@ class TestsCarViews():
         assert response_get_car.status_code == status.HTTP_404_NOT_FOUND
 
         # Car does not exist
-        response_delete_car = api_client.delete(f"/app/cars/{car_id + 1}/",
-                                                format="json")
+        response_delete_car = api_client.delete(
+            f"/app/cars/{car_id + 1}/", format="json"
+        )
         assert response_delete_car.status_code == status.HTTP_404_NOT_FOUND
 
-
-
-
-@pytest.mark.parametrize(
-    ("data", "expected_message"),
-    [
-        ({"phone": "asd1234cvb"}, "Phone number can contain only digits"),
-        ({"phone": "12345678911111"}, "Phone number is too long"),
-        ({"phone": "123"}, "Phone number is too short"),
-        ({"name": "@#$"}, "name can contain only letters and '-' without whitespaces"),
-        (
-            {"surname": "@#$"},
-            "surname can contain only letters and '-' without whitespaces",
-        ),
-        (
-            {"ordering": "invalid ordering"},
-            f"Ordering should be one of the following: ",
-        ),
-    ],
-)
-@pytest.mark.django_db
-def test_owner_request_validation(
-    api_client: APIClient, data: dict[str, str], expected_message: str
-) -> None:
-    ((key, value),) = data.items()
-    if key == "ordering":
-        owner_view = OwnerViewSet()
-        ord_fields_string = ", ".join(owner_view.ordering_fields)
-        expected_message += ord_fields_string
-
-    response_get_owner_invalid_data = api_client.get(
-        "/app/owners/", data=data, format="json"
+    @pytest.mark.parametrize(
+        ("data", "expected_message"),
+        [
+            (
+                {"production_date": datetime.date.today() + datetime.timedelta(days=1)},
+                "Production date cannot be from the future.",
+            ),
+            (
+                {"ordering": "invalid ordering"},
+                f"Ordering should be one of the following: {', '.join(CarViewSet().ordering_fields)}",
+            ),
+        ],
     )
-    assert response_get_owner_invalid_data.status_code == status.HTTP_400_BAD_REQUEST
-    assert response_get_owner_invalid_data.data[key] == expected_message
+    @pytest.mark.django_db
+    def test_car_request_validation(
+        self,
+        api_client: APIClient,
+        data: dict[str, datetime.date | str],
+        expected_message: str,
+    ) -> None:
+        ((key, value),) = data.items()
+        response_get_car_invalid_data = api_client.get(
+            "/app/cars/", data=data, format="json"
+        )
+        assert response_get_car_invalid_data.status_code == status.HTTP_400_BAD_REQUEST
+        assert response_get_car_invalid_data.data[key] == expected_message
 
 
-@pytest.mark.parametrize(
-    ("model_data", "model_str"),
-    [
-        ({"name": "Andrzej", "surname": "Starczyk", "phone": "575849675"}, "owner"),
-        (
-            {
-                "brand": "Ford",
-                "model": "Mondeo",
-                "production_date": "2023-01-01",
-            },
-            "car",
-        ),
-    ],
-)
-@pytest.mark.django_db
-def test_response_no_object_found(
-    api_client: APIClient, model_data: dict[str, str], model_str: str
-) -> None:
-    response_get_model = api_client.get(
-        f"/app/{model_str}s/", data=model_data, format="json"
+class TestsCommon:
+    @pytest.mark.parametrize(
+        ("model_data", "model_str"),
+        [
+            ({"name": "Andrzej", "surname": "Starczyk", "phone": "575849675"}, "owner"),
+            (
+                {
+                    "brand": "Ford",
+                    "model": "Mondeo",
+                    "production_date": "2023-01-01",
+                },
+                "car",
+            ),
+        ],
     )
-    assert response_get_model.status_code == status.HTTP_200_OK
-    assert response_get_model.data == f"There is no {model_str.title()} with given data"
-
-
-@pytest.mark.parametrize(
-    ("data", "expected_message"),
-    [
-        (
-            {"production_date": datetime.date.today() + datetime.timedelta(days=1)},
-            "Production date cannot be from the future.",
-        ),
-        ({"ordering": "invalid ordering"}, "Ordering should be one of the following: "),
-    ],
-)
-@pytest.mark.django_db
-def test_car_request_validation(
-    api_client: APIClient, data: dict[str, datetime.date | str], expected_message: str
-) -> None:
-    ((key, value),) = data.items()
-    if key == "ordering":
-        car_view = CarViewSet()
-        ord_fields_string = ", ".join(car_view.ordering_fields)
-        expected_message += ord_fields_string
-
-    response_get_car_invalid_data = api_client.get(
-        "/app/cars/", data=data, format="json"
-    )
-    assert response_get_car_invalid_data.status_code == status.HTTP_400_BAD_REQUEST
-    assert response_get_car_invalid_data.data[key] == expected_message
+    @pytest.mark.django_db
+    def test_response_no_object_found(
+        self, api_client: APIClient, model_data: dict[str, str], model_str: str
+    ) -> None:
+        response_get_model = api_client.get(
+            f"/app/{model_str}s/", data=model_data, format="json"
+        )
+        assert response_get_model.status_code == status.HTTP_200_OK
+        assert (
+            response_get_model.data
+            == f"There is no {model_str.title()} with given data"
+        )
